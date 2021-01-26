f83be045441d7f8c790ab4cc97d12c1ddf9ac8ca
Akarshit Joshi <joakarshit@gmail.com>
2021-01-27 01:29:33 +0530

    Updates

---
+++ precache-manifest.434eea2975d984bcadd666ce81473790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2fdb491ff3ccdcf326382edb695d6d",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "7c5a7a4a8b7f5e6dbc33",
+    "url": "/BurgerBuilder/static/css/main.68a11a04.chunk.css"
+  },
+  {
+    "revision": "1a33feb2e5098e5a5205",
+    "url": "/BurgerBuilder/static/js/2.e4031565.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/BurgerBuilder/static/js/2.e4031565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5a7a4a8b7f5e6dbc33",
+    "url": "/BurgerBuilder/static/js/main.c885b279.chunk.js"
+  },
+  {
+    "revision": "9b18c247d326418ef3ec",
+    "url": "/BurgerBuilder/static/js/runtime-main.baf6b526.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
